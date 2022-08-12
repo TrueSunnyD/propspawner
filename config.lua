9f36b481c034451237621a536e5e7acbac2d595e
TrueSunnyD <82911075+TrueSunnyD@users.noreply.github.com>
2022-08-12 13:30:57 -0600

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,59 @@
+Config = {
+    Objects = {
+        [1] = { -- Fit N Fab Bench Press 1
+            Hash = 1231502205, -- Prop Hash (Found Here: https://gtahash.ru/)
+            FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+            Coords = vector4(-2990.0, -63.18, 1.77, 68.80) -- Vector 4 Coords for the props location
+        },
+        [2] = { -- Fit N Fab Bench Press 2
+            Hash = 1231502205, -- Prop Hash (Found Here: https://gtahash.ru/)
+            FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+            Coords = vector4(-2989.10, -60.80, 1.77, 68.80) -- Vector 4 Coords for the props location
+        },
+        [3] = { -- Fit N Fab Bench Press 3
+            Hash = 1231502205, -- Prop Hash (Found Here: https://gtahash.ru/)
+            FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+            Coords = vector4(-2988.23, -58.48, 1.77, 68.80) -- Vector 4 Coords for the props location
+        },
+        [4] = { -- Fit N Fab Pull Up Bar 1
+            Hash = -115510932, -- Prop Hash (Found Here: https://gtahash.ru/)
+            FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+            Coords = vector4(-2986.86, -69.62, 1.76, 338.75) -- Vector 4 Coords for the props location
+        },
+        [5] = { -- Fit N Fab Pull Up Bar 2
+            Hash = -115510932, -- Prop Hash (Found Here: https://gtahash.ru/)
+            FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+            Coords = vector4(-2985.12, -70.2, 1.76, 338.75) -- Vector 4 Coords for the props location
+        },
+        [6] = { -- Fit N Fab Pull Up Bar 3
+            Hash = -115510932, -- Prop Hash (Found Here: https://gtahash.ru/)
+            FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+            Coords = vector4(-2983.32, -70.85, 1.76, 338.75) -- Vector 4 Coords for the props location
+        },
+        [7] = { -- Fit N Fab Water Jug 1
+            Hash = -742198632, -- Prop Hash (Found Here: https://gtahash.ru/)
+            FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+            Coords = vector4(-2986.79, -53.09, 1.78, 71.57) -- Vector 4 Coords for the props location
+        },
+        [8] = { -- Fit N Fab Weight Rack 1
+            Hash = 1241057947, -- Prop Hash (Found Here: https://gtahash.ru/)
+            FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+            Coords = vector4(-2987.55, -55.54, 1.77, 68.80) -- Vector 4 Coords for the props location
+        },
+        [9] = { -- Fit N Fab muscle bench 1
+            Hash = -1838046182, -- Prop Hash (Found Here: https://gtahash.ru/)
+            FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+            Coords = vector4(-2981.4, -71.51, 1.76, 338.75) -- Vector 4 Coords for the props location
+        },
+        [10] = { -- Fit N Fab muscle bench 2
+            Hash = -1838046182, -- Prop Hash (Found Here: https://gtahash.ru/)
+            FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+            Coords = vector4(-2979.68, -72.08, 1.76, 338.75) -- Vector 4 Coords for the props location
+        },
+        [11] = { -- Fit N Fab muscle bench 3
+            Hash = -1838046182, -- Prop Hash (Found Here: https://gtahash.ru/)
+            FreezeObj = true, -- Freezes The Prop (Set To true Or false)
+            Coords = vector4(-2978.15, -72.72, 1.76, 338.75) -- Vector 4 Coords for the props location
+        },
+    }
+}
